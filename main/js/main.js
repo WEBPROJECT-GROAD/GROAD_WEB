42bf4569b3dd0291ade8ea194aefac5d0b22d873
안정후 <austinan@naver.com>
2024-11-25 10:49:37 +0900

    feat: mypage side bar

--- main/js/main.js
+++ main/js/main.js
@@ -20,3 +20,18 @@ function showTab2(tabId) {
     document.querySelector(`.tab2[onclick="showTab2('${tabId}')"]`).classList.add('active');
     document.getElementById(tabId).style.display = 'block';
 }
+
+// 사이드 바 마이페이지 함수
+
+function openSidebar() {
+    document.getElementById('myPageSidebar').classList.add('active');
+    document.getElementById('sidebarOverlay').classList.add('active');
+}
+
+function closeSidebar() {
+    document.getElementById('myPageSidebar').classList.remove('active');
+    document.getElementById('sidebarOverlay').classList.remove('active');
+}
+
+document.getElementById('sidebarOverlay').addEventListener('click', closeSidebar);
+
